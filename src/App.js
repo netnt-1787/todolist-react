16fd9c5a79df683cd45f60ae71299708123a8494
netnt-1787 <nguyen.thi.net@sun-asterisk.com>
2019-09-05 17:26:57 +0700

    Todolist-Mock-Source

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return(
+      <div className="todoapp">
+        <div className="container">
+          <header>
+            <h1 className="todoapp__title">todos</h1>
+            <div className="todoapp__input">
+              <input className="todoapp__input-checkbox" type="checkbox"/>
+              <input className="todoapp__input-text" type="text" placeholder="What needs to be done?" />
+            </div>
+          </header>
+          <section>
+            <ul className="todoapp__list">
+              <li className="todoapp__item">
+                <input type="checkbox" className="todoapp__item-checkbox"/>
+                <span className="todoapp__item-context">1</span>
+                <button className="todoapp__item-destroy">x</button>
+              </li>
+              <li className="todoapp__item">
+                <input type="checkbox" className="todoapp__item-checkbox"/>
+                <span className="todoapp__item-context">2</span>
+                <button className="todoapp__item-destroy">x</button>
+              </li>
+            </ul>
+          </section>
+          <footer className="todoapp__footer">
+            <div className="todoapp__total-item">
+              <span>2 item left</span>
+            </div>
+            <div className="todoapp__status">
+              <a href="#!">All</a>
+              <a href="#!">Active</a>
+              <a href="#!">Completed</a>
+            </div>
+            <div className="todoapp__clear">
+              <a href="#!">Clear Completed</a>
+            </div>
+          </footer>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
